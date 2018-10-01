d28831fbed7a34fc6c02c5d485275e9d671b85c6
salma badr <beauty_saloma@outlook.com>
2018-10-01 16:23:55 +0200

    testing heroku

---
+++ src/main/java/Application/Main.java
@@ -0,0 +1,18 @@
+package Application;
+
+import static spark.Spark.*;
+
+public class Main {
+     public static void main(String[] args) {
+         port(getHerokuAssignedPort());
+        get("/hello", (req, res) -> "Hello World");
+       
+    }
+      static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
+}
